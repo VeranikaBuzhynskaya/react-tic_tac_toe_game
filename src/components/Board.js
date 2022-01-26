bcb6faa33eab9acf4ff03439256ecdd0d9fb5413
Veranika Buzhynskaya <veranika.buzhynskaya@gmail.com>
2022-01-26 16:44:30 +0300

    added history of game's moves

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,60 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Square from './Square';
-import calculateWinner from '../helper/calculateWinner';
-import '../index.scss';
 
-class Board extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
+function Board(props) {
+    const renderSquare = (i) => (
+        <Square key={i} value={props.squares[i]} onClick={() => props.onClick(i)}/>
+    );
 
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    renderSquare(i) {
-        return <Square value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}/>
-    }
-
-    render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = (winner) ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-
-        return (
+    return (
             <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {
+                    [...Array(3)].map((elem, i) => (
+                        <div key={i} className="board-row">
+                        {
+                            [...Array(3)].map((elem, j) => renderSquare(3 * i + j))
+                        }
+                        </div>
+                    ))
+                }
             </div>
         )
-    }
 }
 
 export default Board;
